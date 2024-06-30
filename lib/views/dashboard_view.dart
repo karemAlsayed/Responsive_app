4082b43b02231145b6830fa346bb85d26718cd23
karemAlsayed <krym86756@gmail.com>
2024-06-30 10:32:17 +0300

    creatin app_styles

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,14 +1,19 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/widgets/dashboard_desktop_layout.dart';
 
 class DasBoardView extends StatelessWidget {
   const DasBoardView({super.key});
 
+  get desktopLayout => null;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      body: AdaptiveLayout(
+          mobileLayout: (context) => const SizedBox(),
+          tabletLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const DashboardDesktopLayout(),),
     );
   }
 }
