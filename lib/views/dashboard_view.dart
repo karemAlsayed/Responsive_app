522e3a12c3e1be9ae72e941668af95819d91330c
karemAlsayed <krym86756@gmail.com>
2024-06-30 09:43:49 +0300

    first commit

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class DasBoardView extends StatelessWidget {
+  const DasBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
