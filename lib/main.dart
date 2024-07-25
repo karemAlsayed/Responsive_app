4b75a6726ca5dc89a3966bd15df7fa68f475dc17
karemAlsayed <krym86756@gmail.com>
2024-07-25 18:08:00 +0300

    disapling device preview

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:responsive_dashboard/views/dashboard_view.dart';
 
 void main() {
   runApp(DevicePreview(
-    enabled: true,
+    enabled: false,
     builder:(context) => const DashboardApp(),
   ));
 }
